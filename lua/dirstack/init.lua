733aef0b90fafe44a866a52c62f9785bbc358836
phancb <nxhsdu@qq.com>
2023-12-31 03:07:34 +0800

    refactor: reuse some codes

--- lua/dirstack/init.lua
+++ lua/dirstack/init.lua
@@ -3,54 +3,53 @@ local head = {
   next = nil,
   dir = nil,
 }
-
 local node = head
-
 local cd = vim.fn.chdir
 
+local push = function(path)
+  node.next = { next = nil, prev = node, dir = path }
+  node = node.next
+end
+
+local switch_to = function(new_node)
+  if new_node == nil then
+    -- vim.api.nvim_err_writeln "no such node"
+    return
+  end
+  -- NOTE: switch node first, then DirChanged
+  node = new_node
+  local dir = cd(node.dir)
+  -- TODO: dir has been delete
+  if dir then vim.notify(dir .. " -> " .. node.dir) end
+end
+
+local info = function()
+  local it = head
+  local info = ""
+  while it do
+    if it == node then
+      info = info .. "> " .. it.dir .. "\n"
+    else
+      info = info .. "  " .. it.dir .. "\n"
+    end
+    it = it.next
+  end
+  vim.notify(info)
+end
+
 return {
-  info = function()
-    local it = head
-    local info = ""
-    while it do
-      if it == node then
-        info = info .. "> " .. it.dir .. "\n"
-      else
-        info = info .. "  " .. it.dir .. "\n"
-      end
-      it = it.next
-    end
-    vim.notify(info)
-  end,
-  -- TODO: dir may be removed
-  next = function()
-    if node.next then
-      node = node.next
-      local dir = cd(node.dir)
-      vim.notify(dir .. " -> " .. node.dir)
-    else
-      vim.api.nvim_err_writeln "dirlist: no next"
-    end
-  end,
-  prev = function()
-    if node.prev then
-      node = node.prev
-      local dir = cd(node.dir)
-      vim.notify(dir .. " -> " .. node.dir)
-    else
-      vim.api.nvim_err_writeln "dirlist: no prev"
-    end
-  end,
+  info = info,
+  next = function() switch_to(node.next) end,
+  prev = function() switch_to(node.prev) end,
   setup = function()
     node.dir = vim.fn.getcwd()
     vim.api.nvim_create_autocmd("DirChanged", {
       callback = function(ev)
-        local cwd = vim.fn.getcwd()
+        local cwd = ev.file
         -- `prev`/`next` should not trigger `DirChanged`, but set `eventignore` may break other plugins
         -- to determine if we're from `prev`/`next` previously, this hack is sufficient to provide soundness
         if cwd == node.dir then return end
-        node.next = { next = nil, prev = node, dir = cwd }
-        node = node.next
+        push(cwd)
       end,
     })
   end,
